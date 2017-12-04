b68eac12661b6bd09e9f7e24b8b5f21efd5f31e8
jwats287 <jwats287@users.noreply.github.com>
2017-12-03 19:57:10 -0500

    Submit button adds to Ingredient Array and displays on page

--- assets/javascript/recipe.js
+++ assets/javascript/recipe.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-    console.log("Page is loaded")
+    let ingredientArray = ["noodles", "ground beef", "2 cans of tomatoes"];
+
+    addIngredientsToRecipe = () => {
+        $("#ingredients").empty()
+        ingredientArray.forEach((ingredient, index) => {
+            $("#ingredients").append(`<p class="ingredient">${ingredient}</p>`)
+
+        })
+    }
+
+    addIngredientToArray = (newIngredientToAdd) => {
+        ingredientArray.push(newIngredientToAdd);
+    }
+
+    addIngredientsToRecipe();
+})
+
+$(document).on("click", "#add-new-submit", (e) => {
+    e.preventDefault();
+    let newIngredient = $("#add-new-input").eq(0).val().trim();
+    $("#add-new-input").val("");
+    addIngredientToArray(newIngredient);
+    addIngredientsToRecipe();
 })
